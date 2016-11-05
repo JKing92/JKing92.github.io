f668317e57a5d541707e6e94d5821b66ffb64efe
JKing92 <kingj601@gmail.com>
2016-11-05 17:24:16 -0600

    Update
    Introducted modularization and a sitemap. 404 page also included.

---
+++ js/modules.js
@@ -0,0 +1,5 @@
+$(function () {
+    $("#page-nav").load("../homework/navbar.html");
+    $("#page-header").load("../homework/header.html");
+    $("#page-footer").load("../homework/footer.html");
+});
